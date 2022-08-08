12997e26faac940b5f394c77c399cd3b1d9820df
Punchline <re70977za@outlook.com>
2022-08-08 13:33:54 +0430

    Initialized

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,19 @@
 #include"Temp.h"
-int Temp<int>::count = 0;
+int Temp<class a>::count = 0;
+
+template<typename K>
+void print(K n) {
+	cout << n << endl;
+}
 int main() {
 
 	Temp<int> t1(23);
 	t1.print();
+	Temp<string>t2("Hi");
+	t2.print();
+
+	print<string>("44");
+
 	return 0;
+
 }
